7465af35b16908f523f8907d34bc6af5f417f2ed
953506 <62437619+953506@users.noreply.github.com>
2020-03-22 10:53:34 +0300

    Add files via upload

---
+++ chijik.danila/Лаба1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public const double PI = 3.14159265358979;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nWrite data\nExample:Ax^3+Bx^2+Cx+D=0\n");
+            bool exit = false;
+            do
+            {
+                Console.Write("A=");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.Write("B=");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.Write("C=");
+                int C = Convert.ToInt32(Console.ReadLine());
+                Console.Write("D=");
+                int D = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A={0} B={1} C={2} D={3}", A, B, C, D);
+                Console.WriteLine("Is data correct?(Yes or No)");
+                string check = Console.ReadLine();
+                if (check == "Yes")
+                {
+                    double Q = (Pow(B / A, 2) - (3 * (C / A))) / 9;
+                    double R = ((2 * Pow((B / A), 3)) - (9 * (B / A) * (C / A)) + (27 * (D / A))) / 54;
+                    double S = Pow(Q, 3) - Pow(R, 2);
+                    int sgnR = 0;
+                    if (R < 0) { sgnR = -1; }
+                    else if (R > 0) { sgnR = 1; };
+                    if (S > 0)
+                    {
+                        double t = Acos(R / Sqrt(Pow(Q, 3))) / 3;
+                        double x1 = -2 * Sqrt(Q) * Cos(t) - ((B / A) / 3);
+                        double x2 = -2 * Sqrt(Q) * Cos(t + ((2 * PI) / 3)) - ((B / A) / 3);
+                        double x3 = -2 * Sqrt(Q) * Cos(t - ((2 * PI) / 3)) - ((B / A) / 3);
+                        Console.Clear();
+                        Console.WriteLine("\n\nx1 = {0}\nx2 = {1}\nx3 = {2}", x1, x2, x3);
+                    }
+                    if (S < 0)
+                    {
+                        if (Q > 0)
+                        {
+
+                            double t = Log(Abs(R) / Sqrt(Pow(Q, 3))+Sqrt(Pow(Abs(R) / Sqrt(Pow(Q, 3)),2)+1)) / 3;
+                            double x1 = (-2 * sgnR * Sqrt(Q) * Cosh(t)) - ((B / A) / 3);
+                            double x2 = (sgnR * Sqrt(Q) * Cosh(t)) - ((B / A) / 3);
+                            double complex = Sqrt(3) * Sqrt(Q) * Sinh(t);
+                            Console.Clear();
+                            Console.WriteLine("\n\nValid x1 = {0}\nComplex x2 = {1} + {2}*i\nComplex x3 = {3} - {4}*i\n", x1, x2, complex, x2, complex);
+                        }
+                        if (Q < 0)
+                        {
+                            double tt = Abs(R) / Sqrt(Pow(Abs(Q), 3));
+                            double t = Log(tt + Sqrt(Pow(tt,2) - 1)) / 3;
+                            double x1 = (-2 * sgnR * Sqrt(Abs(Q)) * Sinh(t)) - ((B / A) / 3);
+                            double x2 = (sgnR * Sqrt(Abs(Q)) * Sinh(t)) - ((B / A) / 3);
+                            double complex = Sqrt(3) * Sqrt(Abs(Q)) * Cosh(t);
+                            Console.Clear();
+                            Console.WriteLine("\n\nValid x1 = {0}\nComplex x2 = {1} + {2}*i\nComplex x3 = {3} - {4}*i\n", x1, x2, complex, x2, complex);
+                        }
+                        if (Q == 0)
+                        {
+                            double x1 = -Pow((D / A) - (Pow(B / A, 3) / 27) - ((B / A) / 3), 1 / 3);
+                            double x2 = -((B / A) + x1) / 2;
+                            double complex = Sqrt(Abs((((B / A) - 3 * x1) * ((B / A) + x1)) - 4 * (C / A))) / 2;
+                            Console.Clear();
+                            Console.WriteLine("\n\nValid x1 = {0}\nComplex x2 = {1} + {2}*i\nComplex x3 = {3} - {4}*i\n", x1, x2, complex, x2, complex);
+                        }
+
+                    }
+                    if (S == 0)
+                    {
+                        double x1 = (-2 * Pow(R, 1 / 3)) - ((B / A) / 3);
+                        double x2 = (Pow(R, 1 / 3)) - ((B / A) / 3);
+                        Console.Clear();
+                        Console.WriteLine("\n\nx1 = {0}\nx2 = {1}", x1, x2);
+                    };
+                    exit = true;
+                    Console.ReadKey();
+                }
+                else if (check != "No")
+                {
+                    Console.WriteLine("ERROR");
+                };
+
+
+            }
+            while (exit == false);
+        }
+    }
+}
---
+++ chijik.danila/Лаба2/2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while(exit == false)
+            {
+                Console.WriteLine("Choose the task from 1 to 3(for quit - 0):");
+                int choose = Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Press any button to continue...");
+                        exit = true;
+                        break;
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine(DateTime.Now.ToString("dd MMMM yyyy HH:mm:ss\n"));
+                        Console.WriteLine(DateTime.Now.ToString("dd MMMM yyyy hh:mm:ss tt\n"));
+                        string nowDate = DateTime.Now.ToString();
+                        int sum = 0;
+                        for (char num = '0'; num <= '9'; num++)
+                        {
+                            for (int g = 0;g<nowDate.Length ; g++)
+                            { 
+                                if (nowDate[g] == num)
+                                {
+                                    sum++;
+                                }
+                            }
+                            
+                            Console.WriteLine("{0}:{1}",num,sum);
+                            sum = 0;
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Random rnd = new Random();
+                        string[] storage = { "a","b","c","d","e","f","g",
+                                     "h","i","j","k","l","m","n",
+                                     "o","p","q","v","r","s","t",
+                                     "u","v","w","x","y","z"};
+                        Console.Write("Random line: ");
+                        for (int r = 0; r < 4; r++)
+                        {
+                            int letter = rnd.Next(storage.Length);
+                            Console.Write(storage[letter]);
+                        }
+                        Console.Write("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Write interval [a,b]\na=");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("b=");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        BigInteger i;
+                        BigInteger t = 0;
+                        int score = 0;
+                        for (; a <= b; a++)
+                        {
+                            i = a;
+                            t = i % 2;
+                            while (t == 0)
+                            {
+                                if (i%2 == 0)
+                                {
+                                    i /= 2;
+                                    score++;
+                                }
+                                else break;
+                            }
+
+                        }
+                        Console.Write("The highest degree of 2:{0}\n\n", score);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Wrong input");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
